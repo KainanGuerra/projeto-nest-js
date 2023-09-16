f57a3fbce7d48fdc9d05621df604ca61a1cec878
Kainan Guerra <kainan.guerra@frentecorretora.com.br>
2023-09-16 14:49:46 -0300

    fix: found the error on the declaration of the entities on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { UsersModule } from './app/users/users.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + '**/*.entity{.js, .ts}'],
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
       synchronize: true,
     } as TypeOrmModuleOptions),
     UsersModule,
