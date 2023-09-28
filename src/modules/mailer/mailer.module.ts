6a76c8cb59129256cda83986112db47ed44e10e8
Kainan Guerra <kainan.guerra@frentecorretora.com.br>
2023-09-28 19:30:28 -0300

    fix: resolve imports dependencies on module

--- src/modules/mailer/mailer.module.ts
+++ src/modules/mailer/mailer.module.ts
@@ -3,10 +3,11 @@ import { MailerService } from './mailer.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailerEntity } from 'src/entities/mailer.entity';
 import { MailerController } from './mailer.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MailerEntity])],
+  imports: [TypeOrmModule.forFeature([MailerEntity]), HttpModule],
   providers: [MailerService],
   controllers: [MailerController],
 })
-export class MailModule {}
+export class MailerModule {}
